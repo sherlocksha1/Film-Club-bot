9387deb3a0c550071d3f11a83524109a9cba7217
Albin <95078987+Samantha-a@users.noreply.github.com>
2022-03-17 12:43:17 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -94,6 +94,45 @@ def get_text(message: Message) -> [None,str]:
     except IndexError:
         return None
 
+async def progress(current, total, message, start, type_of_ps, file_name=None):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        if elapsed_time == 0:
+            return
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+
+            "".join("▓" for i in range(math.floor(percentage / 10))),
+
+            "".join("ｏ" for i in range(10 - math.floor(percentage / 10))),
+            round(percentage, 2),
+        )
+
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            try:
+                await message.edit(
+                    "{}\n**File Name:** `{}`\n{}".format(type_of_ps, file_name, tmp)
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+        else:
+            try:
+                await message.edit("{}\n{}".format(type_of_ps, tmp))
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+
 
 @Client.on_message(filters.command(["video", "mp4"]))
 async def vsong(client, message: Message):
